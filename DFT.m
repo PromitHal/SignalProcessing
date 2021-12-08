e4b49f11c0583e0de25407e8b575f459f52b95e4
PromitHal <83832850+PromitHal@users.noreply.github.com>
2021-12-08 12:42:27 +0530

    Update DFT.m
    A simple method to calculate DFT of a sequence in MATLAB

--- DFT.m
+++ DFT.m
@@ -1,11 +1,10 @@
 %DISCRETE FOURIER TRANSFORM
-
 %x[n]={x[0],x[1],...x[N-1]}
 %total N samples
 %X[k]=summation(n goes from zero to N-1)x[n]exp(-j*2*pi/N*k*n)
 % k goes from 0 to N-1
 %Input discrete sequence:
-x=[100,400,300,600,900];
+x=[1,2,0.9,0.4,4];
 
 N=length(x);
 %DFT Matrix
@@ -22,3 +21,20 @@ end
 %final DFT
 x_t=transpose(x);
 X=DFT*x_t;%DFT of the input sequence
+%Plotting the DFT coefficients
+
+%Real coefficients
+figure,
+n=[1,2,3,4,5];
+stem(n,real(X),'green');
+xlabel('index of coefficient');
+ylabel('Real part of DFT');
+title('DFT');
+
+%Imaginary coefficients
+figure,
+n=[1,2,3,4,5];
+stem(n,imag(X),'red');
+xlabel('index of coefficient');
+ylabel('Imaginary part of DFT');
+title('DFT','r');
